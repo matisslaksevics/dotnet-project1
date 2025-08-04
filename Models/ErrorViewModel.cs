0785291390e8f39e6599e4bfe02b6f2b3bd5a498
Matiss Laksevics <matiss.laksevics12@gmail.com>
2025-08-04 14:06:06 +0300

    Basic .NET MVC template creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
