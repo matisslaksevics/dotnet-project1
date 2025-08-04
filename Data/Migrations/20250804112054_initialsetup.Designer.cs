197ca8440d2f1c22489cc3a1b055743235a27a60
Matiss Laksevics <matiss.laksevics12@gmail.com>
2025-08-04 14:49:17 +0300

    Basic CRUD logic creation with database

--- Data/Migrations/20250804112054_initialsetup.Designer.cs
+++ Data/Migrations/20250804112054_initialsetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dotnet_project.Data;
 
@@ -11,9 +12,11 @@ using dotnet_project.Data;
 namespace dotnet_project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250804112054_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
